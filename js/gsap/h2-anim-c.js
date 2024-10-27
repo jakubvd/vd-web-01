116d8e3b4d4b028a63f3dbf5fe8739117b619ba3
jakubvd <jakub.michalak@visiondevs.pl>
2024-10-27 23:01:16 +0100

    Update h2-anim-c.js

--- js/gsap/h2-anim-c.js
+++ js/gsap/h2-anim-c.js
@@ -9,29 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
     headings.forEach((heading) => {
         // Hide the original text initially
         const originalText = heading.textContent;
-        heading.textContent = '01'; // Initial binary text
+        heading.style.opacity = '0';
+        heading.textContent = '01_'; // Initial binary text
 
         // Step 1: ScrollTrigger to detect when heading comes into view
         ScrollTrigger.create({
             trigger: heading,
             start: 'top 100%', // Trigger when 100% of the heading is in view
             onEnter: function() {
+                gsap.to(heading, { opacity: 1, duration: 0.1 });
                 // Step 2: Static Binary Text visible for 0.5 seconds
                 gsap.to(heading, {
-                    duration: 0.5,
-                    delay: 0.5, // Show binary text for 0.5 seconds before backspacing
+                    duration: 0.1,
+                    delay: 0.1, // Show binary text for 0.5 seconds before backspacing
                     onComplete: function() {
                         // Step 3: Backspace Effect
                         gsap.to(heading, {
-                            duration: 1.5,
-            text: "",
+                            duration: 0.1,
+                            text: "_",
                             ease: "none",
                             onComplete: function() {
                                 // Step 4: Typing the actual heading content
                                 gsap.to(heading, {
                                     duration: originalText.length * 0.1, // Adjust duration based on text length
                                     text: {
-                                        value: originalText,
+                                        value: originalText + "_",
                                         delimiter: "",
                                     },
                                     ease: "power2.out"

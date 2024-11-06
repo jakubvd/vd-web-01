f98c95b2c87589cabfea4514ead41ac0b95054fd
jakubvd <jakub.michalak@visiondevs.pl>
2024-11-06 03:54:42 +0100

    Update menu-a-2.js

--- js/logic/menu-a-2.js
+++ js/logic/menu-a-2.js
@@ -4,12 +4,12 @@ const menuDiv = document.querySelector('.menu-div');
 
 // Function to show the menu with GSAP animation
 function showMenu() {
-    gsap.to(menuDiv, { right: '0%', duration: 1.0, ease: 'power3.out' }); // Slide menu into view
+    gsap.to(menuDiv, { right: '0%', duration: 1.0, ease: 'power2.out' }); // Slide menu into view
 }
 
 // Function to hide the menu with GSAP animation
 function hideMenu() {
-    gsap.to(menuDiv, { right: '-20%', duration: 0.8, ease: 'power3.in' }); // Slide menu out of view
+    gsap.to(menuDiv, { right: '-20%', duration: 1.0, ease: 'power2.in' }); // Slide menu out of view
 }
 
 // Event listeners for hover functionality

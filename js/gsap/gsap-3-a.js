dbf887e35e331a2a06443e534e3667250bb67592
jakubvd <jakub.michalak@visiondevs.pl>
2024-11-11 04:09:50 +0100

    Update gsap-3-a.js

--- js/gsap/gsap-3-a.js
+++ js/gsap/gsap-3-a.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
         return window.innerWidth <= 767;
     }
 
-    let startValue = isMobile() ? "top 90%" : "top 90%";
+    let startValue = isMobile() ? "top 80%" : "top 90%";
     let endValue = isMobile() ? "bottom 60%" : "bottom 60%";
 
     const sections = [
         { selector: ".section_intro", textClass: ".intro-text-anim", start: startValue, end: endValue },
-        { selector: ".section_outro", textClass: ".outro-text-anim", start: isMobile() ? "top 35%" : "top 90%", end: "bottom center" }
+        { selector: ".section_outro", textClass: ".outro-text-anim", start: isMobile() ? "top 80%" : "top 90%", end: "bottom center" }
     ];
 
     sections.forEach(({ selector, textClass, start, end }) => {

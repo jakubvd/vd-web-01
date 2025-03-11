239297d4cfcf34e431d6267e64fdc8b3336066ff
jakubvd <jakub.michalak@visiondevs.pl>
2025-03-11 17:15:27 +0100

    words fade anim without outro sec

---
+++ js/gsap/gsap-3-b.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function isMobile() {
+        return window.innerWidth <= 767;
+    }
+
+    let startValue = isMobile() ? "top 80%" : "top 90%";
+    let endValue = isMobile() ? "bottom 60%" : "bottom 60%";
+
+    const sections = [
+        { selector: ".section_intro", textClass: ".intro-text-anim", start: startValue, end: endValue },
+    ];
+
+    sections.forEach(({ selector, textClass, start, end }) => {
+        const layoutText = new SplitType(textClass, { types: "words" });
+
+        gsap.from(layoutText.words, {
+            opacity: 0.5,
+            stagger: 0.05,
+            scrollTrigger: {
+                trigger: selector,
+                start: start,
+                end: end,
+                scrub: 2,
+                toggleActions: "restart none none none"
+            }
+        });
+    });
+});
